68126ac1575723fb45c0ff24862847b93ae0e3d1
kevin2-cyber <keduful@st.ug.edu.gh>
2022-03-20 22:55:56 +0000

    created a constants page

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import'package:flutter/material.dart';
+
+const kbgColor = Color(0xFFF3F3F4);
+const kuserColor = Color(0xFFA29BFE);
+const karrowColor = Color(0xFFBEC2C4);
+const kcard1 = Color(0xFFBC5EA4);
+const kcard2 = Color(0xFF16C6FC);
+const kcard3 = Color(0xFFCABAEA);
+const kcard4 = Color(0xFFFF9B1A);
+const kcard3TextColor = Color(0xFFD33135);
+const kcard4TextColor = Color(0xFF725DD6);
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -3,21 +3,14 @@ import 'package:frontend_tryout/details_screen.dart';
 import 'package:frontend_tryout/widgets/expanded_content.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'constants.dart';
+
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
 
   @override
   Widget build(BuildContext context) {
-    const kbgColor = Color(0xFFF3F3F4);
-    const kuserColor = Color(0xFFA29BFE);
-    const karrowColor = Color(0xFFBEC2C4);
-    const kcard1 = Color(0xFFBC5EA4);
-    const kcard2 = Color(0xFF16C6FC);
-    const kcard3 = Color(0xFFCABAEA);
-    const kcard4 = Color(0xFFFF9B1A);
-    const kcard3TextColor = Color(0xFFD33135);
-    const kcard4TextColor = Color(0xFF725DD6);
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 15.0),
